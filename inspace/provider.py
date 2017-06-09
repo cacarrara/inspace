380ebf0c4115d9463c16e955e081df292664be11
Caio Carrara <eu@caiocarrara.com.br>
2017-06-09 00:25:06 -0300

    Simplify galaxy creation

--- inspace/provider.py
+++ inspace/provider.py
@@ -5,12 +5,9 @@ class Provider(object):
     def __init__(self, ):
         self.galaxies = {}
 
-    def create_galaxy(self, name):
-        galaxy = self.get_galaxy(name)
+    def get_galaxy(self, name):
+        galaxy = self.galaxies.get(name)
         if galaxy is not None:
             return galaxy
         self.galaxies[name] = Galaxy({'name': name}, validate=True)
         return self.galaxies[name]
-
-    def get_galaxy(self, name):
-        return self.galaxies.get(name)
--- tests/test_provider.py
+++ tests/test_provider.py
@@ -3,29 +3,18 @@ import pytest
 from schematics.exceptions import ModelValidationError
 
 
-def test_provider_galaxy_create(galaxy_name, provider):
-    galaxy = provider.create_galaxy(galaxy_name)
+def test_provider_galaxy_get_new(galaxy_name, provider):
+    galaxy = provider.get_galaxy(galaxy_name)
     assert galaxy.name == galaxy_name
 
 
-def test_provider_galaxy_create_invalid(provider):
+def test_provider_galaxy_get_invalid(provider):
     with pytest.raises(ModelValidationError):
-        provider.create_galaxy(None)
+        provider.get_galaxy(None)
 
 
-def test_provider_galaxy_create_repeated_name(galaxy_name, provider):
-    first_id = provider.create_galaxy(galaxy_name).id
-    second_id = provider.create_galaxy(galaxy_name).id
+def test_provider_galaxy_get_existent(galaxy_name, provider):
+    first_id = provider.get_galaxy(galaxy_name).id
+    second_id = provider.get_galaxy(galaxy_name).id
     assert len(provider.galaxies) == 1
     assert first_id == second_id
-
-
-def test_provider_galaxy_get_by_name(galaxy_name, provider_loaded):
-    galaxy = provider_loaded.get_galaxy(galaxy_name)
-    assert galaxy
-    assert galaxy.name == galaxy_name
-
-
-def test_provider_galaxy_get_by_name_dont_created(galaxy_name, provider):
-    galaxy = provider.get_galaxy(galaxy_name)
-    assert galaxy is None
