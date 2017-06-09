5725efd85a3f1736b3b489840de7e4bac45aa4d8
Caio Carrara <eu@caiocarrara.com.br>
2017-06-08 22:29:26 -0300

    Changes data structure
    This change puts Resource in Planet and Satellites and removes
    SchemaElement from Resources.

--- inspace/models.py
+++ inspace/models.py
@@ -9,23 +9,23 @@ class IdentifiableModel(Model):
     id = UUIDType(required=True, default=uuid.uuid4())
 
 
-class SchemaElement(IdentifiableModel):
-    name = StringType(required=True)
-
-
 class Resource(IdentifiableModel):
     title = StringType(required=True, max_length=200)
     content = StringType(required=True, max_length=250)
     kind = StringType(required=True, default='text', choices=['text', 'link'])
-    schema_element = ModelType(SchemaElement)
+
+
+class SchemaElement(IdentifiableModel):
+    name = StringType(required=True)
 
 
 class Satellite(SchemaElement):
-    pass
+    resources = ListType(ModelType(Resource))
 
 
 class Planet(SchemaElement):
     satellites = ListType(ModelType(Satellite))
+    resources = ListType(ModelType(Resource))
 
 
 class Star(SchemaElement):
