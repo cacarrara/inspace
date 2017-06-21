4a8e805fb6f1ed8ad5cdf16fd6e53e280d6f301a
ThiagoLopes <thiagolopes@protonmail.com>
2017-06-21 12:10:49 -0300

    first test

--- inspace/tests/core/test_views.py
+++ inspace/tests/core/test_views.py
@@ -1,6 +1,14 @@
 from django.core.urlresolvers import reverse
+import pytest
 
 
 def test_home_get_successfully(client):
     response = client.get(reverse('core:home'))
+    assert response.status_code == 200
+
+
+@pytest.mark.django_db
+def test_resource_list_successfully(client):
+    response = client.get(
+        reverse('core:resource-list', kwargs={'title': 'python'}))
     assert response.status_code == 200
