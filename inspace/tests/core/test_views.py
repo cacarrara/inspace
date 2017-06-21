77895d6268d1571b4e1f36a130bdaeebfa0fc5fe
ThiagoLopes <thiagolopes@protonmail.com>
2017-06-21 11:58:04 -0300

    teste resource_list successfully

--- inspace/tests/core/test_views.py
+++ inspace/tests/core/test_views.py
@@ -4,3 +4,8 @@ from django.core.urlresolvers import reverse
 def test_home_get_successfully(client):
     response = client.get(reverse('core:home'))
     assert response.status_code == 200
+
+
+def test_resource_list_successfully(client):
+    response = client.get(reverse('core:resource-list', {'title': 'python'}))
+    assert response.status_code == 200
