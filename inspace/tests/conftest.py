faa9c3de083cbe8685c6e46e31481d635479dcf4
ThiagoLopes <thiagolopes@protonmail.com>
2017-06-22 19:50:34 -0300

    New fixture

--- inspace/tests/conftest.py
+++ inspace/tests/conftest.py
@@ -1,7 +1,16 @@
 import pytest
 from django.test import Client
+from mixer.backend.django import mixer
+
+from core.models import Resource
 
 
 @pytest.fixture
 def client():
     return Client()
+
+@pytest.fixture
+def resources():
+    titles = ['python', 'ruby', 'php']
+    resources = mixer.cycle(3).blend(Resource, title=(x for x in titles))
+    return Resource
