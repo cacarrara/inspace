469e41962a5a6a0a09e94e2f4549e08d07bc28bf
ThiagoLopes <thiagolopes@protonmail.com>
2017-06-22 19:50:45 -0300

    More tests

--- inspace/tests/core/test_views.py
+++ inspace/tests/core/test_views.py
@@ -9,6 +9,23 @@ def test_home_get_successfully(client):
 
 @pytest.mark.django_db
 def test_resource_list_successfully(client):
+    """test 200 in resource_list"""
     response = client.get(
         reverse('core:resource-list'))
     assert response.status_code == 200
+
+
+@pytest.mark.django_db
+def test_resources_number_all(client, resources):
+    """return all resources"""
+    response = client.get(
+        reverse('core:resource-list'))
+    # will return all resources
+    assert len(response.context['resource_list']) == 3
+
+
+@pytest.mark.django_db
+def test_resouces_query_icontains(client, resources):
+    """will return python and php"""
+    response = client.get('/resources/', {'title':'p'})
+    assert len(response.context['resource_list']) == 2
