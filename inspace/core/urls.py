ed25fe9eb456a82d08aee1ea041a7081211d3d4a
Caio Carrara <eu@caiocarrara.com.br>
2017-06-27 23:55:42 -0300

    Fix urls paths

--- inspace/core/urls.py
+++ inspace/core/urls.py
@@ -4,9 +4,9 @@ from . import views
 
 
 urlpatterns = [
-    url(r'^planets/new/$',
+    url(r'^planet/$',
         views.planet_create_view, name='planet-create'),
-    url(r'^resources/new/$',
+    url(r'^resource/$',
         views.resource_create_view, name='resource-create'),
     url(r'^resources/', views.resource_list_view, name='resource-list'),
     url(r'^$', views.home_view, name='home'),
